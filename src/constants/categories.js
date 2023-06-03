07d29b4f023a5e05886160583bc0c9a29b252480
Edisson <ediloaz@gmail.com>
2023-06-03 11:04:49 -0600

    wip add calories simple to interface

--- src/constants/categories.js
+++ src/constants/categories.js
@@ -2,25 +2,36 @@ export const CATEGORIES = [
   { 
     id: 'protein',
     name: '🥚 Proteínas',
+    kcal: 75,
   },
   { 
     id: 'carb',
     name: '🍞 Harinas',
+    kcal: 80,
   },
   { 
     id: 'dairy',
     name: '🍶 Lácteos',
+    kcal: 100,
   },
   { 
     id: 'fat',
     name: '🥑 Grasas',
+    kcal: 45,
   },
   { 
     id: 'fruit',
     name: '🍍 Frutas',
+    kcal: 60,
   },
   { 
     id: 'vegetable',
     name: '🥒 Vegetales',
+    kcal: 25,
   },
 ]
+
+export const CATEGORIES_OBJECT = CATEGORIES.reduce((obj, category) => {
+  obj[category.id] = category;
+  return obj;
+}, {});
--- src/containers/main/Main.jsx
+++ src/containers/main/Main.jsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from 'react'
+
 import Grid from '@mui/material/Unstable_Grid2'; 
-import { Button, Typography, Paper } from '@mui/material'
-import { SupervisedUserCircle } from '@mui/icons-material'
+import { Button, Typography, Paper, Link } from '@mui/material'
+import { SupervisedUserCircle, Info } from '@mui/icons-material'
 
 // import CircularProgress from '../../components/CircularProgress/CircularProgress';
 import { getBottomBackground } from '../../helpers/images'
 import { getRandomMotivationalPhrase } from '../../helpers/phrases'
+import { CATEGORIES_OBJECT } from '../../constants/categories';
 
 import './main.css'
 
@@ -19,10 +22,50 @@ const CardAction = ({ emoji, title, onClick }) => {
   )
 }
 
-function Main({
+const SumCal = (currentCal) =>  {
+  return parseInt(
+      currentCal?.protein * CATEGORIES_OBJECT?.protein?.kcal +
+      currentCal?.carb * CATEGORIES_OBJECT?.carb?.kcal +
+      currentCal?.dairy * CATEGORIES_OBJECT?.dairy?.kcal +
+      currentCal?.fat * CATEGORIES_OBJECT?.fat?.kcal +
+      currentCal?.fruit * CATEGORIES_OBJECT?.fruit?.kcal +
+      currentCal?.vegetable * CATEGORIES_OBJECT?.vegetable?.kcal
+    )
+}
+
+const CaloriesSummary = ({ plannData, dailyData }) => {
+  return (
+    dailyData && plannData?.kcal
+      ? <Typography variant='caption'>Ha consumido {SumCal(dailyData) || 0 } de {plannData?.kcal}kcal</Typography>
+      : <Typography>...</Typography>
+  )
+}
+
+const Main = ({
   user,
   changeScreen,
-}) {
+  daily,
+  plann,
+  getPlanns,
+  getDaily,
+}) => {
+  const [plannData, setPlannData] = useState({})
+  const [dailyData, setDailyData] = useState({})
+
+  useEffect(() => {
+    const plannQuery = getPlanns(plann)
+    plannQuery.then((data) => {
+      console.log(`Plan actual de ${plann}:`, data)
+      setPlannData(data)
+    })
+
+    const dailyQuery = getDaily(daily)
+    dailyQuery.then((data) => {
+      console.log(`Daily actual de ${daily}:`, data)
+      setDailyData(data)
+    })
+  }, [plann, daily, getPlanns, getDaily])
+  
   const bottomBG = getBottomBackground()
   const randomPhrase = getRandomMotivationalPhrase()
 
@@ -54,6 +97,7 @@ function Main({
         bottomText="calories"
         className="CaloriesCircularProgress"
       /> */}
+      <CaloriesSummary plannData={plannData} dailyData={dailyData} />
       <Grid container spacing={2} className='cardActionContainer' >
         {cardsContent.map((card) => 
           <CardAction key={card?.id} {...card}/>
@@ -68,6 +112,9 @@ function Main({
         <SupervisedUserCircle />
         <span>Cambiar de Usuario ({user})</span>
       </Button>
+      <Link variant="caption" target="_blank" rel="noopener noreferrer" style={{ display: 'flex', zIndex:1 }} href="https://www.studocu.com/pe/document/universidad-femenina-del-sagrado-corazon/nutricion/guia-de-intercambio-ada-con-peso-en-gr-de-alimentos/15508179">
+        <Info fontSize='small' /> Acerca de las porciones
+      </Link>
       <Typography className="phrase">{randomPhrase}</Typography>
       <img
         className="BottomBackground"
